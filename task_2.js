c48db55c5ffddfd3073e96e8000397359fce5c0e
Irina Rusinova <irina.rusinova19@gmail.com>
2023-09-19 15:06:49 +0300

    feature(task 2): do task 2

---
+++ task_2.js
@@ -0,0 +1,13 @@
+function logger() {
+  console.log(`I output only external context: ${this.item}`);
+}
+
+const obj = { item: "some value" };
+
+
+logger.call(obj);
+
+logger.apply(obj);
+
+logger.bind(obj);
+logger();
