e4ae1ecb57e2454bc0867a8a0fdbf7daa8b34171
Irina Rusinova <irina.rusinova19@gmail.com>
2023-09-21 14:30:15 +0300

    feature(task 4): add simple class

---
+++ task_4.js
@@ -0,0 +1,20 @@
+class PersonThree {
+  constructor(name) {
+    this.name = name;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(name) {
+    this._name = name;
+  }
+}
+
+
+class Person extends PersonThree {
+  sayHi() {
+    console.log(`Hi, I am ${this.name}`);
+  }
+}
